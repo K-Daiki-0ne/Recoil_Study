841e8ae582a412e9f1ff07faf327e784eeeb2d4e
0ne <fffdddrrrbbb@gmail.com>
2020-07-04 22:44:14 +0900

    Add: connect Home component

--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import { Home } from '../page/Home';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello Recoil</p>
-    </div>
+    <Home />
   );
 }
 
