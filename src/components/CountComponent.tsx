b059f8c3514ed79ae293507cc55d4d9196be0a60
0ne <fffdddrrrbbb@gmail.com>
2020-07-05 10:45:16 +0900

    Add: Recoil selector in CountComponent

--- src/components/CountComponent.tsx
+++ src/components/CountComponent.tsx
@@ -1,2 +1,13 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { textCountState } from '../store/index';
 
+export function CountComponent() {
+  const count = useRecoilValue(textCountState);
+
+  return (
+    <>
+      Text Count: {count}
+    </>
+  )
+}
