7f72b25aa00b399d8b11047e0e1c9ae78b98da8f
0ne <fffdddrrrbbb@gmail.com>
2020-07-05 22:06:39 +0900

    Add: state types

--- src/store/atom/atom.ts
+++ src/store/atom/atom.ts
@@ -1,6 +1,14 @@
 import { atom } from 'recoil';
 
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const initialTodo: Todo[] = [];
+
 export const todoListState = atom({
   key: 'todoListState',
-  default: [],
+  default: initialTodo,
 })
