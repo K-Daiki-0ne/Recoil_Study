60714861bf7c96f05cdaf237f12f169ebdb6f0ec
0ne <fffdddrrrbbb@gmail.com>
2020-07-07 21:50:06 +0900

    Delete: meaningless type

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -3,7 +3,6 @@ import { useRecoilValue } from 'recoil';
 import { todoListState } from '../../store/atom/atom';
 import { TodoItemCreater } from '../TodoItemCreater/TodoItemCreater';
 import { TodoListItem } from '../TodoListItem/TodoListItem';
-import { Todo } from '../../types/todo';
 
 export function TodoList() {
   const todoList = useRecoilValue(todoListState);
