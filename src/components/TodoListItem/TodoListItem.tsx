cbee0b63426611649fd6b1e6d4fe74aad762c3a0
0ne <fffdddrrrbbb@gmail.com>
2020-07-07 23:58:14 +0900

    Create: TodoListItem Component

--- src/components/TodoListItem/TodoListItem.tsx
+++ src/components/TodoListItem/TodoListItem.tsx
@@ -1,12 +1,53 @@
 import React from 'react'
 import { Todo } from '../../types/todo';
+import { useRecoilState } from 'recoil';
+import { todoListState } from '../../store/atom/atom';
+import { 
+  replaceTodoAtIndex,
+  removeTodoAtIndex 
+} from '../../util/index.js';
 
 type TodoProps = {
   todo: Todo
 }
 
 export const TodoListItem: React.FC<TodoProps> =({ todo })=> {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  const index = todoList.findIndex((listItem) => listItem === todo);
+  
+  const editItemText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newTodo = replaceTodoAtIndex(todoList, index, {
+      ...todo,
+      title: event.target.value
+    });
+    setTodoList(newTodo);
+  };
+
+  const toggleItemCompletion= () => {
+    const newTodo = replaceTodoAtIndex(todoList, index, {
+      ...todo,
+      completed: !todo.completed,
+    });
+    setTodoList(newTodo);
+  }
+
+  const deleteItem = () => {
+    const newList = removeTodoAtIndex(todoList, index);
+
+    setTodoList(newList);
+  }
+
+
+
   return (
-    <p>Hello TodoItem Component</p>
+    <div>
+      <input type='text' value={todo.title} onChange={editItemText} />
+      <input 
+        type='checkbox'
+        checked={todo.completed}
+        onChange={toggleItemCompletion}
+      />
+      <button onClick={deleteItem}>✖️</button>
+    </div>
   )
 }
