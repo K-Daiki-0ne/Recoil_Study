c7c92082724110bda4bcf1943e7f62200cd70cfd
0ne <kouguchi3456@yahoo.co.jp>
2020-07-12 22:52:11 +0900

    fix: division to ecide filter

--- src/store/selector/selector.ts
+++ src/store/selector/selector.ts
@@ -3,6 +3,20 @@ import {
   todoListState,
   todoListFilterState
 } from '../atom/atom';
+import { Todo } from '../../types/todo';
+
+const getVisibleTodo = (list: Todo[], filter: string) => {
+  switch(filter) {
+    case 'Show All':
+      return list;
+    case 'Show Completed':
+      return list.filter((todo) => todo.completed);
+    case 'Show UnCompleted':
+      return list.filter((todo) => !todo.completed);
+    default:
+      throw new Error(`Sorry Not ${filter}`);
+  }
+}
 
 export const filterTodoListState = selector({
   key: 'filterTodoListState',
@@ -10,14 +24,7 @@ export const filterTodoListState = selector({
     const filetr = get(todoListFilterState);
     const list = get(todoListState);
     
-    switch(filetr) {
-      case 'Show Completed':
-        return list.filter((item) => item.completed);
-      case 'Show UnCompleted':
-        return list.filter((item) => !item.completed);
-      default:
-        return list;
-    }
+    getVisibleTodo(list, filetr);
   }
 })
 
