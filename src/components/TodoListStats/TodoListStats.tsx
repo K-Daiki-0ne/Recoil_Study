b6f50135a030c652c40484bbc396ac1a7a64f04c
0ne <kouguchi3456@yahoo.co.jp>
2020-07-12 13:27:48 +0900

    add: output todo number & completed todo number

--- src/components/TodoListStats/TodoListStats.tsx
+++ src/components/TodoListStats/TodoListStats.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { todoListStatsState } from '../../store/selector/selector';
 import { Paper } from '@material-ui/core';
+import { useStyle } from './style';
 
 export function TodoListStats() {
   const { 
@@ -11,14 +12,21 @@ export function TodoListStats() {
     percentCompleted
   } = useRecoilValue(todoListStatsState);
 
+  const formattedPercentCompleted: number = Math.round(percentCompleted * 100);
+
   return (
     <div>
       <Paper>
         Total items: {totalNum}
       </Paper>
-
       <Paper>
-
+        Items Completed: {totalCompletedNum}
+      </Paper>
+      <Paper>
+        Items Not Completes: {totalUnCompletedNum}
+      </Paper>
+      <Paper>
+        Percent Completed: {formattedPercentCompleted}
       </Paper>
     </div>
   )
