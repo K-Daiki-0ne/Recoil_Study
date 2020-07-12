9fac8ba4edc07c753cb5d65e0376e534c9fd7d6d
0ne <kouguchi3456@yahoo.co.jp>
2020-07-12 10:26:32 +0900

    add: get select data function

--- src/store/selector/selector.ts
+++ src/store/selector/selector.ts
@@ -1,1 +1,22 @@
 import { selector } from 'recoil';
+import { 
+  todoListState,
+  todoListFilterState
+} from '../atom/atom';
+
+const filterTodoListState = selector({
+  key: 'filterTodoListState',
+  get: ({ get }) => {
+    const filetr = get(todoListFilterState);
+    const list = get(todoListState);
+    
+    switch(filetr) {
+      case 'Show Completed':
+        return list.filter((item) => item.completed);
+      case 'Show UnCompleted':
+        return list.filter((item) => !item.completed);
+      default:
+        return list;
+    }
+  }
+})
