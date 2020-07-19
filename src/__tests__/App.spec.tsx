07f64c2929bd3340de9ce72283985dfa33c37e19
0ne <kouguchi3456@yahoo.co.jp>
2020-07-19 23:04:41 +0900

    add: before test function

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
 import App from '../root/App';
+
+beforeEach(cleanup);
 
 describe('App component', () => {
   test('Render Test App', () => {
--- src/__tests__/Home.spec.tsx
+++ src/__tests__/Home.spec.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import { Home } from '../page/Home';
+
+beforeEach(cleanup);
 
 describe('pages/Home', () => {
   test('Rendering test Home', () => {
