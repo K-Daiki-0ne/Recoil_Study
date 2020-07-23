b56c9fb6c73196d3612696b3c9100dfc372b9ff7
0ne <kouguchi3456@yahoo.co.jp>
2020-07-24 01:56:59 +0900

    fix: TodoListItem Test file

--- src/__tests__/TodoListItem.test.tsx
+++ src/__tests__/TodoListItem.test.tsx
@@ -3,11 +3,18 @@ import { TodoListItem } from '../components/TodoListItem/TodoListItem';
 import Enzyme, { shallow } from 'enzyme';
 import ReactSixteenAdapter from 'enzyme-adapter-react-16';
 
+import { Todo } from '../types/todo';
+
 Enzyme.configure({ adapter: new ReactSixteenAdapter() });
 
-// describe('Redering Test', () => {
-//   test('Reders Test for TodoItemCreater for TodoItemCreater Component', () => {
-//     const wrapper = shallow(<TodoListItem />);
-//     expect(wrapper).toMatchSnapshot();
-//   })
-// })
+describe('Redering Test', () => {
+  test('Reders Test for TodoItemCreater for TodoItemCreater Component', () => {
+    const TestData: Todo = {
+      id: '1',
+      title: 'This is test data',
+      completed: true
+    };
+    const wrapper = shallow(<TodoListItem todo={TestData}/>);
+    expect(wrapper).toMatchSnapshot();
+  })
+})
