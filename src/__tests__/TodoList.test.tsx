4329c861ebb9737682b7d30dedbecf4a8ab613da
0ne <kouguchi3456@yahoo.co.jp>
2020-07-24 01:40:23 +0900

    create: TodoList Test file

---
+++ src/__tests__/TodoList.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TodoList } from '../components/TodoList/TodoList';
+import Enzyme, { shallow } from 'enzyme';
+import ReactSixteenAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new ReactSixteenAdapter() });
+
+describe('Redering Test', () => {
+  test('Reders Test for TodoItemCreater for TodoItemCreater Component', () => {
+    const wrapper = shallow(<TodoList />);
+    expect(wrapper).toMatchSnapshot();
+  })
+})
