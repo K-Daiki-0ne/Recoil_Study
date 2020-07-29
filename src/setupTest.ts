9edb3a307ceb556fef150aa8afeccc713d253ee1
0ne <kouguchi3456@yahoo.co.jp>
2020-07-29 21:59:27 +0900

    fix & delete: jest version setUptest

--- src/setupTest.ts
+++
@@ -1,8 +0,0 @@
-import Enzyme from 'enzyme';
-import ReactSixteenAdapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new ReactSixteenAdapter() });
-
-
-// テストのため一時package.jsonのscriptをコメントアウトするための避難場所
-// "jest": "react-scripts test --coverage --runInBand --watchAll=false",
